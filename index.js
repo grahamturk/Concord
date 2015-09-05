6af1598049ade8288d4147df4035e96f68fd41ca
Sierra Yit <sierrayit@gmail.com>
2015-09-05 14:31:11 -0400

    Keeps track of type of food and calculates the most popular.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SlackBot = require('slackbots');
 var conf = require('nconf');
-
+var channel;
 conf.file({ file: 'config.json' });
 
 // create a bot
@@ -9,29 +9,61 @@ var bot = new SlackBot({
   name: conf.get('name')
 });
 
+var startWords = ["lunch", "order", "dinner", "breakfast", "brunch"];
+
+var containsStartWord = function(message) {
+    for (var word in startWords) {
+        if (message.indexOf(word) >= 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // Checks if lunch was used in message, starts LunchBot.
 bot.on('message', function(data) {
    if (data.type === 'message' && data.subtype !== 'bot_message') {
        var message = data.text;
-    if (message.indexOf("lunch") >= 0 || message.indexOf("order") >= 0) {
-        startLunchBot();
+       channel = data.getChannel;
+    if (containsStartWord()) {
+        getTypeOfFood();
     }
    }
 });
-
-function startLunchBot() {
-    getTypeOfFood();
-};
 
 function getTypeOfFood() {
     var users = bot.getUsers()._value.members;
     for (var userIndex in users) {
         var username = users[userIndex].name;
         if (username !== 'concord' && username !== 'slackbot') {
-            bot.postMessageToUser(username,
-                "What kind of food would you like? Please respond within 15 minutes.", {});
+            bot.postMessageToChannel(username,
+                "What kind of food would you like (Eg. Chinese, Italian)? Please respond within 15 minutes.", {});
         }
     }
+    var typeOfFood = {};
+    var topFood = { type: [], count: 0 };
+    bot.on('message', function(data) {
+       if (data.type === 'message' && data.subtype !== 'bot_message' && data.channel == channel) {
+           var message = data.text.toLowerCase();
+           // TODO: account for differences in strings like adding "food" to the end by removing spaces
+           if (message.indexOf("food") >= 0) {
+               message = message.substring(0,message.length - 5);
+           }
+           if (typeOfFood[message] != null) {
+               typeOfFood[message]++;
+               var currCount = typeOfFood[message];
+               if (topFood.count < currCount) {
+                   topFood.type = [message];
+                   topFood.count = currCount;
+               } else if (topFood.count == currCount) {
+                   topFood.type[topFood.type.length + 1] = message;
+               }
+           } else {
+               typeOfFood[message] = 1;
+           }
+       }
+       return topFood.type;
+    });
 };
 
 
