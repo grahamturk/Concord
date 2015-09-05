6e59f6b332f2abfd29579397feb07a9b244d1cab
Sierra Yit <sierrayit@gmail.com>
2015-09-05 11:48:06 -0400

    Starts bot and adds prompts for type of food.

--- index.js
+++ index.js
@@ -9,8 +9,29 @@ var bot = new SlackBot({
   name: conf.get('name')
 });
 
+// Checks if lunch was used in message, starts LunchBot.
 bot.on('message', function(data) {
-  if (data.type === 'message' && data.subtype !== 'bot_message') {
-    console.log(data);
-  }
+   if (data.type === 'message' && data.subtype !== 'bot_message') {
+       var message = data.text;
+    if (message.indexOf("lunch") >= 0 || message.indexOf("order") >= 0) {
+        startLunchBot();
+    }
+   }
 });
+
+function startLunchBot() {
+    getTypeOfFood();
+};
+
+function getTypeOfFood() {
+    var users = bot.getUsers()._value.members;
+    for (var userIndex in users) {
+        var username = users[userIndex].name;
+        if (username !== 'concord' && username !== 'slackbot') {
+            bot.postMessageToUser(username,
+                "What kind of food would you like? Please respond within 15 minutes.", {});
+        }
+    }
+};
+
+
